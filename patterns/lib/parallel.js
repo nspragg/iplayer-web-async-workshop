102e931cd9bf507bc0cd4e36a033aad27e58c925
nick spragg <5319732+nspragg@users.noreply.github.com>
2018-03-23 10:24:43 +0000

    Task: Remove for loop.

--- patterns/lib/parallel.js
+++ patterns/lib/parallel.js
@@ -1,30 +1,24 @@
 'use strict';
 
-const _ = require('lodash');
-
 async function handleArray(iterable) {
   return await Promise.all(iterable.map((fn) => fn()));
 }
 
 async function handleObject(iterable) {
-  const obj = {};
+  const pending = [];
+  const results = {};
   for (const k of Object.keys(iterable)) {
     const fn = iterable[k];
-    obj[k] = fn();
+    pending.push(fn().then((v) => {
+      results[k] = v;
+    }));
   }
-  await Promise.all(Object.values(obj));
+  await Promise.all(Object.values(pending));
 
-  return resolveAll(obj);
-}
-
-async function resolveAll(obj) {
-  for (const [k, v] of Object.entries(obj)) {
-    obj[k] = await v;
-  }
-  return obj;
+  return results;
 }
 
 module.exports = async (iterable) => {
-  if (_.isArray(iterable)) return handleArray(iterable);
+  if (Array.isArray(iterable)) return handleArray(iterable);
   return handleObject(iterable);
 };
